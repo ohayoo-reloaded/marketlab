3f5e76a79a8bc2db8380b23a3d8582cc8c8e980f
Otto Bossmann <otto.kostmann@hotmail.com>
2024-07-24 15:15:32 +0200

    starting on modal
    Co-authored-by: nilshansson <nilsmenghansson@gmail.com>

---
+++ src/app/@modal/(.)item/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { Modal } from './modal';
+import { FullPageImageView } from '~/app/common/full-page-image-view';
+
+export default function PhotoModal({
+  params: { id: photoId },
+}: {
+  params: { id: string };
+}) {
+
+  const idAsNumber = Number(photoId)
+  if(Number.isNaN(idAsNumber)) throw new Error("Invalid photo id")
+
+  return (
+  <div>
+    <Modal>
+    <FullPageImageView photoId={photoId}/>
+    </Modal>
+  </div>
+  )
+
+}
