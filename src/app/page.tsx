60d774662fcc4e0ec776b01d281f552ad4d8e693
Nils Hansson <nilsmenghansson@gmail.com>
2024-07-24 15:12:47 +0200

    created form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import { getItems } from "@/server/query";
 import Image from "next/image";
+import { AddItemForm } from "./_components/_add-items-modal/product-form";
 
 export default async function Home() {
-
-  const items = await getItems()
+  const items = await getItems();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
-      Welcome to this amazing market!
-      </h1>
+      <h1>Welcome to this amazing market!</h1>
       <div>
         {items.length > 0 ? (
           items.map((item, index) => (
@@ -19,6 +17,7 @@ export default async function Home() {
           <div>Loading...</div>
         )}
       </div>
+      <AddItemForm />
     </main>
   );
 }
